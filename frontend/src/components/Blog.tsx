91a9e20f6f40f958caee574a362fd69e5234a402
rparakh2004 <rparakh_be22@thapar.edu>
2025-02-04 00:35:02 +0530

    bug fix

--- frontend/src/components/Blog.tsx
+++ frontend/src/components/Blog.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import parse from "html-react-parser";
+import {convert} from "html-to-text";
+import dompurify from "dompurify";
 
 interface BlogProps {
   title: string;
@@ -10,9 +12,8 @@ interface BlogProps {
 }
 
 const Blog: React.FC<BlogProps> = ({ title, content, author, date, onClick }) => {
-    const text = parse(content);
-    // const text = convert(content,{wordwrap:false});
-    // console.log(text.slice(0,100));
+
+    const text = convert(content,{wordwrap:false});
   return (
     <div onClick={onClick} className="w-3/4 mx-auto py-6 border-b">
       <div  className="flex items-center gap-2 text-gray-600 text-sm">
@@ -21,7 +22,7 @@ const Blog: React.FC<BlogProps> = ({ title, content, author, date, onClick }) =>
 
       <h2 className="text-xl font-bold mt-1">{title}</h2>
 
-      <div className="text-gray-600 mt-1">{text}</div>
+      <div className="text-gray-600 mt-1">{text.slice(0,100)}</div>
       <div className="text-gray-500 text-sm mt-2">{date.slice(0,10)}</div>
     </div>
   );
